bd07b3cad33c732e390627ea15311c04ad97bd88
Marion Craipeau <marioncraipeau@Pierres-MacBook.local>
2017-10-08 23:20:18 +0200

    ADD [Errors explanation] on form

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,3 +1,25 @@
 class Cup < ApplicationRecord
-  validates :title, presence: true
+
+  # Configurations =============================================================
+
+  # Associations ===============================================================
+
+  # Callbacks ==================================================================
+
+  validates :title, :start_at, :end_at, presence: true
+  validate :validate_dates
+
+  # Scopes =====================================================================
+
+  # Class Methods ==============================================================
+
+  # Instance Methods ===========================================================
+
+  private #=====================================================================
+
+  def validate_dates
+    if start_at < Time.zone.today || end_at < Time.zone.today || start_at >= end_at
+      errors.add(:base, "Les dates de la compétition doivent être valides")
+    end
+  end
 end
