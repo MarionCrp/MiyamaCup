a2a60d36a5ba05f907eb8eb22a99e395989358ad
Marion Craipeau <marioncraipeau@Pierres-MacBook.local>
2017-10-30 23:12:22 +0100

    SETUP [User devise]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, path: 'admin', path_names: { sign_in: 'login'}
+  devise_for :users
   devise_for :admin, path: 'admin', path_names: { sign_in: 'login'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
