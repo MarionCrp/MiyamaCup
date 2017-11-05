e794975bb33951e3eaeb343f79ae129e88650436
Marion Craipeau <marioncraipeau@Pierres-MacBook.local>
2017-11-05 15:09:31 +0100

    ADD [User::UserProfile edit/update]

--- app/controllers/user/user_profiles_controller.rb
+++ app/controllers/user/user_profiles_controller.rb
@@ -1,4 +1,6 @@
 class User::UserProfilesController < User::BaseController
+
+  before_action :find_user_profile, only: [:edit, :update]
   def index
     @main_profile = current_user.main_profile
     @child_profiles = current_user.child_profiles
@@ -19,7 +21,24 @@ class User::UserProfilesController < User::BaseController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user_profile.update(user_profile_params)
+      flash[:success] = t('.success')
+      redirect_to user_user_profiles_path
+    else
+      flash[:error] = t('.error')
+      render 'edit', status: 422
+    end
+  end
+
   private
+
+  def find_user_profile
+    @user_profile = current_user.user_profiles.find(params[:id])
+  end
 
   def user_profile_params
     params.require(:user_profile).permit(:is_main_profile, :firstname, :lastname, :gender, :grade, :club, :birthdate)
