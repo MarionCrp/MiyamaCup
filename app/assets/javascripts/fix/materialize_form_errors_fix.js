b6cbacff61df7559aea7f4805bc78a32e4788cad
Marion Craipeau <marioncraipeau@Pierres-MacBook.local>
2017-11-05 18:17:47 +0100

    FIX [Materialize fields_with_error] sometimes the label is also rounded by fields_with_errors

--- app/assets/javascripts/fix/materialize_form_errors_fix.js
+++ app/assets/javascripts/fix/materialize_form_errors_fix.js
@@ -1,5 +1,6 @@
 document.addEventListener("turbolinks:load", function () {
   document.body.addEventListener("ajax:error", function (e) {
     $('.field_with_errors, .datepicker').siblings('label').addClass('active')
+    $('.field_with_errors').children('label').addClass('active')
   });
 });
